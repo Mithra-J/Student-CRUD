b43536e6da822d424cf2f80cd1c70c72308657c5
Mithra J <mithraacct@gmail.com>
2025-04-09 22:56:08 +0530

    Create db_connect.php

---
+++ db_connect.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";   // or 127.0.0.1
+$username = "root";           // default XAMPP phpMyAdmin username
+$password = "";               // default XAMPP phpMyAdmin password is empty
+$database = "cruddb";         // your database name
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
+

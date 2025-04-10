4dd913b2adbd71d4b81a443a744c911ac139f183
Mithra J <mithraacct@gmail.com>
2025-04-10 21:24:16 +0530

    Update add.php

--- add.php
+++ add.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+
     <title>Add New Student</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>

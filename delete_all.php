1574b2d7c24d7687938c4c63784890c6ce4c6d5a
Mithra J <mithraacct@gmail.com>
2025-04-29 21:46:28 +0530

    Update delete_all.php

--- delete_all.php
+++ delete_all.php
@@ -1,11 +1,14 @@
 <?php
 include 'db_connect.php';
 
-$query = "DELETE FROM students";
-if (mysqli_query($conn, $query)) {
-    header("Location: index.php"); // Redirect back to main page
+mysqli_query($conn, "SET FOREIGN_KEY_CHECKS = 0");
+$success = mysqli_query($conn, "TRUNCATE TABLE students");
+mysqli_query($conn, "SET FOREIGN_KEY_CHECKS = 1");
+
+if ($success) {
+    header("Location: index.php");
     exit();
 } else {
-    echo "Error deleting all students: " . mysqli_error($conn);
+    echo "Error truncating students table: " . mysqli_error($conn);
 }
 ?>
